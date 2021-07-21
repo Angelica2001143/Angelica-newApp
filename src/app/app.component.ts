4f765c7dba6dba662c35ee9fdda8d65d5168dfcb
Angelica2001143 <bibiangelicaputot@gmail.com>
2021-07-22 06:20:03 +0800

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angelica-newApp';
+  appMessage = "";
+
+
+  appEvent(event: any){
+    this.appMessage=event;
+  }
 }
